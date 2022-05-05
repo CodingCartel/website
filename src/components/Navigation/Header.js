6dbc92eb7c348f408ffdf1b40b977b957316e241
iKeaght <noelmatheo@yahoo.fr>
2022-05-05 15:47:57 +0200

    Add header to see if everything is working

---
+++ src/components/Navigation/Header.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Header = () => {
+  return <div>je suis le Header</div>
+}
+
+export default Header
--- src/components/blocks/HeroBlock.js
+++ src/components/blocks/HeroBlock.js
@@ -1,10 +1,12 @@
 import React from 'react'
 // import styles from './HeroBlock.module.scss'
+import Header from '../Navigation/Header'
 
 const HeroBlock = () => {
   return (
     <section>
-      <h1>Bienvenue sur le coding carte </h1>
+      <h1>Bienvenue sur le coding cartel </h1>
+      <Header />
     </section>
   )
 }
